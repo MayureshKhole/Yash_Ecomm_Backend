41f6e754d29c7a6a51eaeba03f456b158f2aa652
yashanbhore <anbhorey2003@gmail.com>
2024-06-18 16:43:09 +0530

    Backend Deployed 9 - cors options

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const router = require("./routes")
+const router = require("./routes");
 const connectDB = require("./config/db");
-const cookieParser = require('cookie-parser')
-
+const cookieParser = require('cookie-parser');
 
 const app = express();
-app.use(cors({
-  origin : process.env.FRONTEND_URL,
-  credentials : true
-}))
+
+// Define CORS options
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || '*',
+  credentials: true,
+};
+
+// Set up CORS middleware with defined options
+app.use(cors(corsOptions));
+
+// Handle OPTIONS requests
 app.options('*', cors(corsOptions));   // this is the options request (https://chatgpt.com/c/4130e902-b69c-46f5-ac7d-65dcc270daa2)
-app.use(cookieParser())
-app.use(express.json())
-app.use("/api",router)
+
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api", router);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-
-
-
 const PORT = 8080 || process.env.PORT;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log("Mongodb Connected");
+    console.log("MongoDB Connected");
     console.log("SERVER IS RUNNING AT PORT : ", PORT);
   });
 });
-
-
-//Test3@gmail.com 12345678
